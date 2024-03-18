a76f63f780b34372412f15c0d4bf5729bad609dd
Tungle <tung291512@gmail.com>
2024-03-19 00:41:12 +0700

    tung

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,12 +1,86 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
- 
-  const { email, password } = req.body;
+  const { email, password } = req.body
   if (!email || !password) {
     return res.status(400).json({
       error: 'Thiếu email hoặc mật khẩu'
-    });
+    })
   }
-  next();
-};
+  next()
+}
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        }
+      },
+      trim: true
+    },
+    email: {
+      notEmpty: true,
+      trim: true,
+      isEmail: true
+    },
+    password: {
+      notEmpty: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      }
+    },
+    confirm_password: {
+      notEmpty: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password')
+          }
+          return true
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+)
--- src/routes/users.routers.ts
+++ src/routes/users.routers.ts
@@ -1,9 +1,17 @@
-import { Router } from "express"
-import { loginController,registerController } from "~/controllers/users.controllers"
-import { loginValidator } from "~/middlewares/users.middlewares"
+import { Router } from 'express'
+import { loginController, registerController } from '~/controllers/users.controllers'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 const usersRouter = Router()
 
-usersRouter.post('/login', loginValidator,loginController)
-usersRouter.post('/register', registerController)
+usersRouter.post('/login', loginValidator, loginController)
+/**
+ * Description. Register a new user
+ * Path: /register
+ * Method: POST
+ * body: {name: string, email: string, password: string, conform_password: string,
+ * date_of_birth: ISO8601}
+ */
+
+usersRouter.post('/register',registerValidator, registerController)
 
 export default usersRouter
