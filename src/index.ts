e02e72ce4464b5e2c10acb401e5c672628e1afaa
Tungle <tung291512@gmail.com>
2024-03-29 23:20:34 +0700

    wraprequesthandler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import usersRouter from './routes/users.routers'
 import databaseService from '~/services/database.services'
 const app = express()
@@ -6,7 +6,7 @@ const port = 3000
 app.use(express.json())
 app.use('/users', usersRouter)
 databaseService.connect()
-app.use((err, req, res, next) => {
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.log(err)
   res.status(400).json({
     error: err.message
